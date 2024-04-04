339061fee0a40d12d96a5fb61bacb63a059a90e7
초짜맨 <yongseung.song@icloud.com>
2024-04-04 10:42:33 +0900

    initial project setup

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import Page from '@/components/Page';
+
+function HomePage() {
+  return <Page>Hello, world!</Page>;
+}
+
+export default HomePage;
---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,5 @@
+function Page({ children }: { children: React.ReactNode }) {
+  return <main className="container py-20 px-5 mx-auto">{children}</main>;
+}
+
+export default Page;
